150a7e763f1c3fa7e545606eb1345091ac10a4b6
Hemendra2007 <hemendraroy09@gmail.com>
2024-08-21 00:37:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+let board = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+
+function handleCellClick(event) {
+    const cell = event.target;
+    const cellIndex = cell.id.split("-")[1]; 
+
+    if (board[cellIndex] !== "") {
+        return;
+    }
+
+    board[cellIndex] = currentPlayer;
+    cell.textContent = currentPlayer;
+
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+
+    console.log(board); 
+}
+
+
+document.querySelectorAll(".cell").forEach(cell => {
+    cell.addEventListener("click", handleCellClick);
+});
+
+console.log("Tic Tac Toe script initialized!");
